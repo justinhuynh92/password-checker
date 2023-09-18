9bc6971e4eb33f3b7976ff14f946618fb3d952a8
Justin Huynh <justinhuynh639@gmail.com>
2023-09-18 16:46:02 -0700

    pw checker 2

--- checkmypass.py
+++ checkmypass.py
@@ -10,9 +10,19 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
     return res
 
+#check what kind of data we're receiving
+def get_password_leaks_count(hashes, hash_to_check):
+    #return a list of the lines in the string, breaking at line boundaries
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        print(h, count)
+
 #check password if it exists in API response
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(response)
+    return get_password_leaks_count(response, tail)
 
 pwned_api_check('123')
